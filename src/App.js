fb9aeb0516d0cdbdcb6d1759f9e45c2e1c48e8e7
Jair Cordero <cordero1823.cr@gmail.com>
2020-05-06 21:13:40 -0600

    Test build in host

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Futuro portafolio personal.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/jair1823"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Luis Jair Cordero Barona
         </a>
       </header>
     </div>
